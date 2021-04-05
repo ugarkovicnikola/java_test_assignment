19d7c88b41e6d62badee43252d466c00f483f413
Nikola Ugarkovic <nikola.ugarkovic@yahoo.com>
2021-04-05 20:25:49 +0200

    Add FunctionalTest and PageObject

--- src/test/java/setup/FunctionalTest.java
+++ src/test/java/setup/FunctionalTest.java
@@ -1,13 +1,16 @@
+package setup;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * <strong>FunctionalTest</strong> handles setup and teardown of WebDriver.
+ * <strong>setup.FunctionalTest</strong> handles setup and teardown of WebDriver.
  * @author Kim Schiller
  */
 public class FunctionalTest {
@@ -16,7 +19,8 @@ public class FunctionalTest {
 
   @BeforeClass
   public static void setUp(){
-    driver = new FirefoxDriver();
+    System.setProperty("webdriver.chrome.driver", "E:\\Alas doo project\\java_test_assignment\\src\\chromedriver.exe");
+    driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
   }
 
--- src/test/java/setup/PageObject.java
+++ src/test/java/setup/PageObject.java
@@ -1,3 +1,5 @@
+package setup;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
